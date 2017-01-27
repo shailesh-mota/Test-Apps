071d667624fe9569f54211b5fc2bdbf77736e3eb
Shailesh Mota <shailesh.mota@fwd.cloud>
2017-01-27 22:29:05 +0200

    Add new events that can be used in future.

--- Code/BarScanner/app/src/main/java/fun/mota/com/barscanner/event/CommandEvent.java
+++ Code/BarScanner/app/src/main/java/fun/mota/com/barscanner/event/CommandEvent.java
@@ -7,7 +7,11 @@ package fun.mota.com.barscanner.event;
 
 public class CommandEvent extends Event {
     public enum Type {
-        UPLOAD_SCAN_RESULT             // Quickly upload collected scan results
+        UPLOAD_SCAN_RESULT_SUCCESS,            // Upload collected scan results, success
+        UPLOAD_SCAN_RESULT_FAILED,             // Upload collected scan results, failure
+        SEND_USER_DATA,                        // Upload user data
+        SENDING_USER_DATA_SUCCESS,             // Upload user data, success
+        SENDING_USER_DATA_FAILURE              // Upload user data, failure
     }
 
     public CommandEvent(Type type) {
